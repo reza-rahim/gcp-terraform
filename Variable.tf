4994a9bcaeb0db2c18f5824bf4863aa050724ac4
Reza Rahim <rezarahim@Rezas-MacBook-Pro.local>
2020-02-03 22:42:03 -0600

    init

--- Variable.tf
+++ Variable.tf
@@ -1,27 +1,27 @@
-
 variable "credentials" {
-        default = "../../gcpservice/terraform_account.json"
-    }
-
-variable "var_project" {
-        default = "redislabs-sa-training-services"
-    }
-
-variable "region" {
-        default = "us-central1"
+  default = "../../gcpservice/terraform_account.json"
 }
 
- variable "zone" {
-        default = "us-central1-a"
-}   
+variable "var_project" {
+  default = "redislabs-sa-training-services"
+}
 
- variable "vpc" {
-        default = "kube"
-}   
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "vpc" {
+  default = "kube"
+}
 
 variable "gce_ssh_user" {
   default = "root"
 }
+
 variable "gce_ssh_pub_key_file" {
   default = "~/.ssh/id_rsa.pub"
 }
@@ -42,7 +42,6 @@ variable "ubuntu" {
   default = "ubuntu-os-cloud/ubuntu-1804-lts"
 }
 
-
 variable "boot_disk_size" {
   default = 50
 }
@@ -54,10 +53,10 @@ variable "bastion_machine_type" {
 variable "controller_machine_count" {
   default = 2
 }
+
 variable "controller_machine_type" {
   default = "n1-standard-4"
 }
-
 
 variable "worker_machine_count" {
   default = 2
@@ -67,10 +66,10 @@ variable "worker_machine_type" {
   default = "n1-standard-4"
 }
 
-
 variable "storage_machine_count" {
   default = 2
 }
+
 variable "storage_machine_type" {
   default = "n1-standard-4"
 }
@@ -82,6 +81,4 @@ variable "storage_disk_size" {
 variable "storage_disk_type" {
   default = "pd-standard"
 }
-
-
 
--- main.tf
+++ main.tf
@@ -1,34 +1,32 @@
 provider "google" {
-  credentials = "${file("${var.credentials}")}"
-  project     = "${var.var_project}"
-  region      = "${var.region}"
-  zone        = "${var.zone}"
+  credentials = file(var.credentials)
+  project     = var.var_project
+  region      = var.region
+  zone        = var.zone
 }
 
 resource "google_compute_network" "vpc" {
-  name          =  "${var.vpc}"
+  name                    = var.vpc
   auto_create_subnetworks = "false"
   routing_mode            = "GLOBAL"
 }
 
-
 resource "google_compute_subnetwork" "public-subnet" {
-  name            = "public-subnet"
-  network         = "${google_compute_network.vpc.name}"
-  ip_cidr_range   = "${var.gce_public_subnet_cidr}"
+  name          = "public-subnet"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = var.gce_public_subnet_cidr
 }
 
 resource "google_compute_subnetwork" "private-subnet" {
-  name            = "private-subnet"
-  network         = "${google_compute_network.vpc.name}"
-  ip_cidr_range   = "${var.gce_private_subnet_cidr}"
+  name          = "private-subnet"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = var.gce_private_subnet_cidr
 }
-
 
 resource "google_compute_router" "router" {
   name    = "router"
-  region  = "${google_compute_subnetwork.private-subnet.region}"
-  network = "${google_compute_network.vpc.self_link}"
+  region  = google_compute_subnetwork.private-subnet.region
+  network = google_compute_network.vpc.self_link
   bgp {
     asn = 64514
   }
@@ -36,16 +34,15 @@ resource "google_compute_router" "router" {
 
 resource "google_compute_router_nat" "simple-nat" {
   name                               = "nat-1"
-  router                             = "${google_compute_router.router.name}"
-  region                             = "${var.region}"
+  router                             = google_compute_router.router.name
+  region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-
 resource "google_compute_firewall" "private-firewall" {
   name    = "private-firewall"
-  network = "${google_compute_network.vpc.name}"
+  network = google_compute_network.vpc.name
 
   allow {
     protocol = "icmp"
@@ -59,12 +56,12 @@ resource "google_compute_firewall" "private-firewall" {
     protocol = "tcp"
   }
 
-  source_ranges = [ "${var.gce_public_subnet_cidr}","${var.gce_private_subnet_cidr}" ]
+  source_ranges = [var.gce_public_subnet_cidr, var.gce_private_subnet_cidr]
 }
 
 resource "google_compute_firewall" "public-firewall" {
   name    = "public-firewall"
-  network = "${google_compute_network.vpc.name}"
+  network = google_compute_network.vpc.name
 
   allow {
     protocol = "icmp"
@@ -78,26 +75,27 @@ resource "google_compute_firewall" "public-firewall" {
     protocol = "tcp"
   }
 
-  source_ranges = [ "0.0.0.0/0" ]
+  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_address" "bastion-ip-address" {
-  count = "${var.bastion-ip-address-count}"
-  name = "bastion-ip-address-${count.index}"
+  count = var.bastion-ip-address-count
+  name  = "bastion-ip-address-${count.index}"
 }
 
 resource "google_compute_instance" "bastion" {
-  count = "${var.bastion-ip-address-count}"
-  name            = "bastion-${count.index}"
-  machine_type    = "${var.bastion_machine_type}"
+  count        = var.bastion-ip-address-count
+  name         = "bastion-${count.index}"
+  machine_type = var.bastion_machine_type
+
   #can_ip_forward  = true
 
-  tags = ["kubernetes-the-easy-way","bastion"]
+  tags = ["kubernetes-the-easy-way", "bastion"]
 
   boot_disk {
     initialize_params {
-      image = "${var.ubuntu}"
-      size  = "${var.boot_disk_size}"
+      image = var.ubuntu
+      size  = var.boot_disk_size
     }
   }
 
@@ -106,7 +104,8 @@ resource "google_compute_instance" "bastion" {
   }
 
   network_interface {
-    subnetwork = "${google_compute_subnetwork.public-subnet.name}"
+    subnetwork = google_compute_subnetwork.public-subnet.name
+
     #network_ip = "10.10.0.${count.index+2}"
 
     access_config {
@@ -115,37 +114,36 @@ resource "google_compute_instance" "bastion" {
   }
 
   service_account {
-    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
   }
 
   metadata_startup_script = "apt-get install -y python"
 
   metadata = {
-    sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}" 
+    sshKeys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
 
-  
   network_interface {
     network = "default"
     access_config {
-      nat_ip = "${google_compute_address.bastion-ip-address[count.index].address}"
+       nat_ip  = element(google_compute_address.bastion-ip-address.*.address, count.index)
     }
   }
-
 }
 
 resource "google_compute_instance" "controller" {
-  count = "${var.controller_machine_count}"
-  name            = "controller-${count.index}"
-  machine_type    = "${var.controller_machine_type}"
+  count        = var.controller_machine_count
+  name         = "controller-${count.index}"
+  machine_type = var.controller_machine_type
+
   #can_ip_forward  = true
 
-  tags = ["kubernetes-the-easy-way","controller"]
+  tags = ["kubernetes-the-easy-way", "controller"]
 
   boot_disk {
     initialize_params {
-      image = "${var.ubuntu}"
-      size  = "${var.boot_disk_size}"
+      image = var.ubuntu
+      size  = var.boot_disk_size
     }
   }
 
@@ -154,13 +152,12 @@ resource "google_compute_instance" "controller" {
   }
 
   network_interface {
-    subnetwork = "${google_compute_subnetwork.private-subnet.name}"
+    subnetwork = google_compute_subnetwork.private-subnet.name
     #network_ip = "10.10.0.${count.index+4}"
-
   }
 
   service_account {
-    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
   }
 
   metadata = {
@@ -170,20 +167,19 @@ resource "google_compute_instance" "controller" {
   metadata_startup_script = "apt-get install -y python"
 }
 
-
-
 resource "google_compute_instance" "worker" {
-  count = "${var.worker_machine_count}"
-  name            = "worker-${count.index}"
-  machine_type    = "${var.worker_machine_type}"
+  count        = var.worker_machine_count
+  name         = "worker-${count.index}"
+  machine_type = var.worker_machine_type
+
   #can_ip_forward  = true
 
-  tags = ["kubernetes-the-easy-way","worker"]
+  tags = ["kubernetes-the-easy-way", "worker"]
 
   boot_disk {
     initialize_params {
-      image = "${var.ubuntu}"
-      size  = "${var.boot_disk_size}"
+      image = var.ubuntu
+      size  = var.boot_disk_size
     }
   }
 
@@ -192,13 +188,12 @@ resource "google_compute_instance" "worker" {
   }
 
   network_interface {
-    subnetwork = "${google_compute_subnetwork.private-subnet.name}"
+    subnetwork = google_compute_subnetwork.private-subnet.name
     #network_ip = "10.20.0.${count.index+3}"
-
   }
 
   service_account {
-    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
   }
 
   metadata = {
@@ -209,33 +204,34 @@ resource "google_compute_instance" "worker" {
 }
 
 resource "google_compute_disk" "storage-disk-b-" {
-    count   = "${var.storage_machine_count}"
-    name    = "storage-disk-b-${count.index}-data"
-    type    = "${var.storage_disk_type}"
-    zone    = "${var.zone}"
-    size    = "${var.storage_disk_size}"
+  count = var.storage_machine_count
+  name  = "storage-disk-b-${count.index}-data"
+  type  = var.storage_disk_type
+  zone  = var.zone
+  size  = var.storage_disk_size
 }
 
 resource "google_compute_disk" "storage-disk-c-" {
-    count   = "${var.storage_machine_count}"
-    name    = "storage-disk-c-${count.index}-data"
-    type    = "${var.storage_disk_type}"
-    zone    = "${var.zone}"
-    size    = "${var.storage_disk_size}"
+  count = var.storage_machine_count
+  name  = "storage-disk-c-${count.index}-data"
+  type  = var.storage_disk_type
+  zone  = var.zone
+  size  = var.storage_disk_size
 }
 
 resource "google_compute_instance" "storage" {
-  count = "${var.storage_machine_count}"
-  name            = "storage-${count.index}"
-  machine_type    = "${var.storage_machine_type}"
+  count        = var.storage_machine_count
+  name         = "storage-${count.index}"
+  machine_type = var.storage_machine_type
+
   #can_ip_forward  = true
 
-  tags = ["kubernetes-the-easy-way","storage"]
+  tags = ["kubernetes-the-easy-way", "storage"]
 
   boot_disk {
     initialize_params {
-      image = "${var.ubuntu}"
-      size  = "${var.boot_disk_size}"
+      image = var.ubuntu
+      size  = var.boot_disk_size
     }
   }
 
@@ -244,13 +240,12 @@ resource "google_compute_instance" "storage" {
   }
 
   network_interface {
-    subnetwork = "${google_compute_subnetwork.private-subnet.name}"
+    subnetwork = google_compute_subnetwork.private-subnet.name
     #network_ip = "10.20.0.${count.index+3}"
-
   }
 
   service_account {
-    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+    scopes = ["compute-rw", "storage-ro", "service-management", "service-control", "logging-write", "monitoring"]
   }
 
   metadata = {
@@ -258,14 +253,15 @@ resource "google_compute_instance" "storage" {
   }
 
   attached_disk {
-        source      = "${element(google_compute_disk.storage-disk-b-.*.self_link, count.index)}"
-        device_name = "${element(google_compute_disk.storage-disk-b-.*.name, count.index)}"
-   }
+    source      = element(google_compute_disk.storage-disk-b-.*.self_link, count.index)
+    device_name = element(google_compute_disk.storage-disk-b-.*.name, count.index)
+  }
 
   attached_disk {
-        source      = "${element(google_compute_disk.storage-disk-c-.*.self_link, count.index)}"
-        device_name = "${element(google_compute_disk.storage-disk-c-.*.name, count.index)}"
-   }
+    source      = element(google_compute_disk.storage-disk-c-.*.self_link, count.index)
+    device_name = element(google_compute_disk.storage-disk-c-.*.name, count.index)
+  }
 
   metadata_startup_script = "apt-get install -y python"
 }
+
